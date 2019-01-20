8dad7c1798fcc3c77928e825499b151d71550619
Tomoya Gibo <e175734@ie.u-ryukyu.ac.jp>
2019-01-20 22:52:33 +0900

    JavaDocドキュメントを記述

--- src/main/java/jp/ac/uryukyu/ie/e175734/LivingThing.java
+++ src/main/java/jp/ac/uryukyu/ie/e175734/LivingThing.java
@@ -1,5 +1,15 @@
 package jp.ac.uryukyu.ie.e175734;
 
+/**
+ * ゲームの敵キャラや味方キャラを生成する際のスーパークラス。
+ *
+ * String name; //名前。
+ * int hitPoint; //体力。
+ * int attack; //攻撃力。
+ * int now_hitPoint; //現在の体力。
+ * boolean dead; //生死状態。true = 死んでいる状態。
+ * boolean escape; //逃走状態。true = 逃走に成功した状態。
+ */
 class LivingThing {
     String name;
     int hitPoint;
@@ -8,6 +18,14 @@ class LivingThing {
     boolean dead;
     boolean escape;
 
+    /**
+     * コンストラクタ。名前，体力，攻撃力を指定する。
+     * 指定された体力を元に現在の体力の初期値を設定する。
+     *
+     * @param name 名前。
+     * @param hitPoint 体力。
+     * @param attack 攻撃力。
+     */
     LivingThing(String name, int hitPoint, int attack){
         this.name = name;
         this.hitPoint = hitPoint;
@@ -19,6 +37,13 @@ class LivingThing {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
     }
 
+    /**
+     * 攻撃するメソッド。
+     * 相手が死亡状態，または逃走状態でない時に攻撃する。
+     * 体力が５割未満の時，３０％の確率で通常の１．５倍の大ダメージを与える。
+     *
+     * @param livingThing 本クラス。ここでは，攻撃する相手になる。
+     */
     void attack(LivingThing livingThing){
         int damage;
         double random;
@@ -40,8 +65,14 @@ class LivingThing {
         }
     }
 
+    /**
+     * ダメージ処理をするメソッド。
+     * 体力が０以下の時，死亡状態にする。
+     *
+     * @param damage 受けたダメージ。
+     */
     void wounded(int damage){
-        now_hitPoint = now_hitPoint - damage;
+        now_hitPoint -= damage;
 
         if (now_hitPoint <= 0) {
             dead = true;
@@ -49,6 +80,13 @@ class LivingThing {
         }
     }
 
+    /**
+     * 逃走判定を行うメソッド。
+     * 死亡状態でない，かつ逃走に成功している状態でない時，逃走に成功した状態にする。
+     *
+     * @param random 乱数。逃走の成功確率をこれで判定する。
+     * @param perHitPoint 現在の体力をパーセンテージで表したもの。
+     */
     void escape(double random, double perHitPoint) {
         if (dead == false && escape == false) {
             if (random <= 0.3 && perHitPoint <= 0.3) {
